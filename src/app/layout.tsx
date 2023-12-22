67194639801142109378dfcc0d84cc304553ebc9
troyfung <fungtroy63@gmail.com>
2023-12-23 02:34:15 +0800

    adjust

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,10 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <NavigationBar/>
-      {children}
       <Analytics />
       <SpeedInsights/>
+      <NavigationBar/>
+      {children}
       </body>
     </html>
   )
