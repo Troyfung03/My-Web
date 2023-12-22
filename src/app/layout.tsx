68597bb254e0e94933067adf6033f2b5c65ef6ad
troyfung <fungtroy63@gmail.com>
2023-12-23 02:35:59 +0800

    comment analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <Analytics />
-      <SpeedInsights/>
+      {/* <Analytics />
+      <SpeedInsights/> */}
       <NavigationBar/>
       {children}
       </body>
